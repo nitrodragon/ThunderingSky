7cd5b6e8a1cfc2eebfd10dabfabde73fc07e882f
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-04-10 21:39:39 -0700

    Update BaseCharacter.java

--- src/BaseCharacter.java
+++ src/BaseCharacter.java
@@ -1,13 +1,20 @@
 // import nothing
 
+import static java.lang.Math.floor;
+
 public class BaseCharacter {
 
     private String characterClass;
-    private int HP;
-    private int ATK;
-    private int DEF;
-    private int MAG;
-    private int SPD;
+
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+
+    public int HP;
+    public int ATK;
+    public int DEF;
+    public int MAG;
+    public int SPD;
 
     public BaseCharacter(String characterClass, int HP, int ATK, int DEF, int MAG, int SPD) {
         this.characterClass = characterClass;
@@ -16,7 +23,6 @@ public class BaseCharacter {
         this.DEF = DEF;
         this.MAG = MAG;
         this.SPD = SPD;
-
     }
 
     public String getCharacterClass() {
@@ -42,4 +48,25 @@ public class BaseCharacter {
     public int getSPD() {
         return SPD;
     }
+
+    public void setHP(int base, int level, int iv, int ev) {
+        this.HP = (int) floor((((base + iv) * 2 + ((Math.sqrt(ev) / 4))) * level) / 100) + level + 10;
+    }
+
+    public void setATK(int base, int level, int iv, int ev) {
+        this.ATK = (int) floor((((base + iv) * 2 + ((Math.sqrt(ev) / 4))) * level) / 100) + 5;
+    }
+
+    public void setDEF(int base, int level, int iv, int ev) {
+        this.DEF = (int) floor((((base + iv) * 2 + ((Math.sqrt(ev) / 4))) * level) / 100) + 5;
+    }
+
+    public void setMAG(int base, int level, int iv, int ev) {
+        this.MAG = (int) floor((((base + iv) * 2 + ((Math.sqrt(ev) / 4))) * level) / 100) + 5;
+    }
+
+    public void setSPD(int base, int level, int iv, int ev) {
+        this.SPD = (int) floor((((base + iv) * 2 + ((Math.sqrt(ev) / 4))) * level) / 100) + 5;
+    }
+
 }
