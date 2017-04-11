74ddc6fa0906559c72b65fb2146ac262f3bae2d4
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-04-10 21:39:09 -0700

    Update BasesGalore.java

--- src/BasesGalore.java
+++ src/BasesGalore.java
@@ -3,20 +3,42 @@
  */
 public interface BasesGalore {
 
-    String[] classes = {
-            "villager", // 0-0-0-0 (base class)
-            "fighter", "guard", "spellcaster", "athlete", // 1 stat point
-            "knight", "spellsword", "martial_artist", "cleric", "escapist", "expediter", "slayer", "defender", "magician", "guerrilla" // 2 stat points
-    };
+    static final int VILLAGER = 0;
+    static final int FIGHTER = 1;
+    static final int GUARD = 2;
+    static final int SPELLCASTER = 3;
+    static final int ATHLETE = 4;
+    static final int KNIGHT = 5;
+    static final int SPELLSWORD = 6;
+    static final int MARTIALARTIST = 7;
+    static final int CLERIC = 8;
+    static final int ESCAPIST = 9;
+    static final int EXPEDITER = 10;
+    static final int SLAYER = 11;
+    static final int DEFENDER = 12;
+    static final int MAGICIAN = 13;
+    static final int GUERRILLA = 14;
 
-    static final BaseCharacter[] characters = {
-            new BaseCharacter("villager", 50, 50, 50, 50, 50),
-            new BaseCharacter("fighter", 60, 70, 50, 35, 35),
-            new BaseCharacter("guard", 80, 50, 80, 20, 40),
-            new BaseCharacter("spellcaster", 65, 20, 15, 105, 60),
-            new BaseCharacter("athlete", 65, 50, 50, 20, 80),
-            new BaseCharacter("knight", 80, 70, 50, 35, 35),
 
+    // The comments here signify the average stats for each class. 40 for Class 0, 51-53 for Class 1, 68-72 for Class 2, TBD (90?) for Class 3.
+    BaseCharacter[] characters = {
+            new BaseCharacter("Villager", 40, 40,40, 40,40), // 40
+
+            new BaseCharacter("Fighter", 65, 70, 50, 35, 35), // 51
+            new BaseCharacter("Guard", 80, 50, 80, 20, 40), // 53
+            new BaseCharacter("Spellcaster", 60, 20, 15, 105, 60), // 53
+            new BaseCharacter("Athlete", 65, 50, 50, 20, 80), // 52
+
+            new BaseCharacter("Knight", 90, 85, 85, 35, 45), // 69
+            new BaseCharacter("Spellsword", 75, 75, 30, 130, 45), // 71
+            new BaseCharacter("Martial_artist", 80, 85, 60, 40, 90), // 71
+            new BaseCharacter("Cleric", 90, 30, 75, 110, 40), // 69
+            new BaseCharacter("Escapist", 85, 45, 85, 35, 90), // 68
+            new BaseCharacter("Expediter", 70, 60, 85, 100, 45), // 72
+            new BaseCharacter("Slayer", 65, 120, 65, 35, 65), // 70
+            new BaseCharacter("Defender", 115, 70, 120, 25, 30), // 72
+            new BaseCharacter("Magician", 65, 55, 40, 150, 40), // 71
+            new BaseCharacter("Guerrilla", 85, 80, 65, 15, 110), // 71
 
     };
 }
