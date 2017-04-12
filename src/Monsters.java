52c1c0fabcadfd6562f55331d8db6cc7aba9d74a
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-04-12 08:37:22 -0700

    Update Monsters.java

--- src/Monsters.java
+++ src/Monsters.java
@@ -4,6 +4,7 @@ public interface Monsters {
     int ZOMBIE = 2;
 
     BaseMonster[] monsters = {
+            // So, are we gonna have a static HP for the Monsters? No calculation. That could work. I have a damage formula, so we'll have to put that in here soon.
             new BaseMonster("Slime", 30, 5, 2, 10),
             new BaseMonster("Bat", 20, 10, 2, 30),
             new BaseMonster("Zombie", 60, 7, 5, 5)
