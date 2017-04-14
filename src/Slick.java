41e94c7f82b3382c3ea2df6d2afac31a698e5ab3
Ethan Barker <ethanbarker0@gmail.com>
2017-04-13 17:56:24 -0700

    Slick testing! Will update readme with a tutorial

---
+++ src/Slick.java
@@ -0,0 +1,32 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Slick extends StateBasedGame {
+
+    public static final String gamename = "Thundering Sky";
+    public static final int play = 0;
+    public static final int xSize = 400;
+    public static final int ySize = 300;
+
+    public Slick(String gamename){
+        super(gamename);
+        this.addState(new Play());
+    }
+
+    public void initStatesList(GameContainer gc) throws SlickException{
+        this.getState(play).init(gc, this);
+        this.enterState(play);
+    }
+
+    public static void main(String[] args) {
+        AppGameContainer appgc;
+        try{
+            appgc = new AppGameContainer(new Slick(gamename));
+            appgc.setDisplayMode(xSize, ySize, false);
+            appgc.setTargetFrameRate(60);
+            appgc.start();
+        }catch(SlickException e){
+            e.printStackTrace();
+        }
+    }
+}
