6218df3ac804870389d8e85e28ed9caeb2d42ebd
Ethan Barker <ethanbarker0@gmail.com>
2017-04-16 19:35:35 -0700

    Add some docs and quick improvements

---
+++ src/com/thunder/sky/GUI/Menu.java
@@ -0,0 +1,102 @@
+package com.thunder.sky.GUI;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.command.*;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.*;
+import java.awt.Font;
+import org.newdawn.slick.TrueTypeFont;
+import com.thunder.sky.Player;
+
+/**
+ * Main menu screen. Currently only used to
+ * input stats that are printed to console.
+ */
+
+public class Menu extends BasicGameState implements InputProviderListener {
+
+    private float frameCount = 0;
+    private TrueTypeFont active;
+    private TrueTypeFont inactive;
+    private String[] statNames = {"Attack: ", "Defence: ", "Magic: ", "Speed: "};
+    private int[] statValues = {0, 0, 0, 0};
+    private BasicCommand up = new BasicCommand("up");
+    private BasicCommand down = new BasicCommand("down");
+    private BasicCommand left = new BasicCommand("left");
+    private BasicCommand right = new BasicCommand("right");
+    private BasicCommand enter = new BasicCommand("enter");
+    private int pointer = 0; // Which stat is being edited
+    private Rectangle[] rect = new Rectangle[4];
+    private StateBasedGame sb;
+
+    public void init(GameContainer gc, StateBasedGame sbg)
+            throws SlickException {
+        sb = sbg;
+        InputProvider provider = new InputProvider(gc.getInput());
+        provider.addListener(this);
+        Font finactive = new Font("Helvetica", Font.PLAIN, 20);
+        Font factive = new Font("Helvetica", Font.BOLD, 24);
+        active = new TrueTypeFont(factive, true);
+        inactive = new TrueTypeFont(finactive, true);
+        provider.bindCommand(new KeyControl(Input.KEY_UP), up);
+        provider.bindCommand(new KeyControl(Input.KEY_DOWN), down);
+        provider.bindCommand(new KeyControl(Input.KEY_LEFT), left);
+        provider.bindCommand(new KeyControl(Input.KEY_RIGHT), right);
+        provider.bindCommand(new KeyControl(Input.KEY_ENTER), enter);
+
+        for (int i = 0; i < rect.length; i++) {
+            rect[i] = new Rectangle(100, 20 + (70 * i), statValues[i] * 40, 20);
+        }
+    }
+
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+            throws SlickException {
+        for (int i = 0; i < statNames.length; i++) {
+            if (pointer == i) {
+                // + (float)Math.sin(frameCount * 25)
+                active.drawString(20, 20 + (70 * i), statNames[i] + statValues[i]);
+            } else {
+                inactive.drawString(20, 20 + (70 * i), statNames[i]);
+            }
+//            g.setColor(Color.white);
+//            g.fill(rect[i]);
+            if (statValues[i] > 0) g.draw(rect[i]);
+        }
+    }
+
+    public void update(GameContainer gc, StateBasedGame sbg, int delta)
+            throws SlickException {
+        frameCount += 0.01;
+    }
+
+    @Override
+    public void controlPressed(Command command) {
+        if (command.toString().equals("[Command=down]")) {
+            if (pointer < 3) pointer++;
+        } else if (command.toString().equals("[Command=up]")){
+            if (pointer > 0) pointer--;
+        } else if (command.toString().equals("[Command=right]")) {
+            if (statValues[pointer] < 3) {
+                statValues[pointer]++;
+                rect[pointer].setWidth(statValues[pointer] * 40);
+            }
+        } else if (command.toString().equals("[Command=left]")) {
+            if (statValues[pointer] > 0) {
+                statValues[pointer]--;
+                rect[pointer].setWidth(statValues[pointer] * 40);
+            }
+        } else {
+            Player player = new Player(statValues[0], statValues[1], statValues[2], statValues[3]);
+            sb.enterState(2);
+        }
+    }
+
+    @Override
+    public void controlReleased(Command command) {
+
+    }
+
+    public int getID() {
+        return 0;
+    }
+}
--- src/com/thunder/sky/GUI/Play.java
+++ src/com/thunder/sky/GUI/Play.java
@@ -1,101 +1,45 @@
 package com.thunder.sky.GUI;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.command.*;
-import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.*;
-import java.awt.Font;
-import org.newdawn.slick.TrueTypeFont;
-import com.thunder.sky.Player;
 
-public class Play extends BasicGameState implements InputProviderListener {
+/**
+ * Entry point for GUI version of the game.
+ * Runs Play.java using set parameters inside
+ * this file.
+ */
 
-    private float frame = 0;
-    private TrueTypeFont active;
-    private TrueTypeFont inactive;
-    private String[] statNames = {"Attack: ", "Defence: ", "Magic: ", "Speed: "};
-    private int[] statValues = {0, 0, 0, 0};
-    private BasicCommand up = new BasicCommand("up");
-    private BasicCommand down = new BasicCommand("down");
-    private BasicCommand left = new BasicCommand("left");
-    private BasicCommand right = new BasicCommand("right");
-    private BasicCommand enter = new BasicCommand("enter");
-    private int pointer = 0;
-    private Rectangle[] rect = new Rectangle[4];
-    StateBasedGame sb;
 
-    Play() {
+public class Play extends StateBasedGame {
 
+    private static final String gamename = "Thundering Sky";
+    private static final int play = 0;
+    private static final int xSize = 400;
+    private static final int ySize = 300;
+    private static final int fps = 60;
+    private static final boolean showFps = true;
+
+    private Play(String gamename) {
+        super(gamename);
+        this.addState(new Menu());
+        this.addState(new Game());
     }
 
-    public void init(GameContainer gc, StateBasedGame sbg)
-            throws SlickException {
-        sb = sbg;
-        InputProvider provider = new InputProvider(gc.getInput());
-        provider.addListener(this);
-        Font finactive = new Font("Helvetica", Font.PLAIN, 20);
-        Font factive = new Font("Helvetica", Font.BOLD, 24);
-        active = new TrueTypeFont(factive, true);
-        inactive = new TrueTypeFont(finactive, true);
-        provider.bindCommand(new KeyControl(Input.KEY_UP), up);
-        provider.bindCommand(new KeyControl(Input.KEY_DOWN), down);
-        provider.bindCommand(new KeyControl(Input.KEY_LEFT), left);
-        provider.bindCommand(new KeyControl(Input.KEY_RIGHT), right);
-        provider.bindCommand(new KeyControl(Input.KEY_ENTER), enter);
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.enterState(play);
+    }
 
-        for (int i = 0; i < rect.length; i++) {
-            rect[i] = new Rectangle(100, 20 + (70 * i), statValues[i] * 40, 20);
+    public static void main(String[] args) {
+        AppGameContainer gameContainer;
+        try {
+            gameContainer = new AppGameContainer(new Play(gamename));
+            gameContainer.setShowFPS(showFps);
+            gameContainer.setVSync(true); // Should probably stay true for performance
+            gameContainer.setDisplayMode(xSize, ySize, false);
+            gameContainer.setTargetFrameRate(fps);
+            gameContainer.start();
+        } catch(SlickException e) {
+            e.printStackTrace();
         }
-    }
-
-    public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-            throws SlickException {
-        for (int i = 0; i < statNames.length; i++) {
-            if (pointer == i) {
-                // + (float)Math.sin(frame * 25)
-                active.drawString(20, 20 + (70 * i), statNames[i] + statValues[i]);
-            } else {
-                inactive.drawString(20, 20 + (70 * i), statNames[i]);
-            }
-//            g.setColor(Color.white);
-//            g.fill(rect[i]);
-            if (statValues[i] > 0) g.draw(rect[i]);
-        }
-    }
-
-    public void update(GameContainer gc, StateBasedGame sbg, int delta)
-            throws SlickException {
-        frame += 0.01;
-    }
-
-    @Override
-    public void controlPressed(Command command) {
-        if (command.toString().equals("[Command=down]")) {
-            if (pointer < 3) pointer++;
-        } else if (command.toString().equals("[Command=up]")){
-            if (pointer > 0) pointer--;
-        } else if (command.toString().equals("[Command=right]")) {
-            if (statValues[pointer] < 3) {
-                statValues[pointer]++;
-                rect[pointer].setWidth(statValues[pointer] * 40);
-            }
-        } else if (command.toString().equals("[Command=left]")) {
-            if (statValues[pointer] > 0) {
-                statValues[pointer]--;
-                rect[pointer].setWidth(statValues[pointer] * 40);
-            }
-        } else {
-            Player player = new Player(statValues[0], statValues[1], statValues[2], statValues[3]);
-            sb.enterState(2);
-        }
-    }
-
-    @Override
-    public void controlReleased(Command command) {
-
-    }
-
-    public int getID() {
-        return 0;
     }
 }
